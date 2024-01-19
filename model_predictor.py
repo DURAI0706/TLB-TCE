2dce9d3a1fecb58549843b6b257dd66b1280ead4
K-Senthil-Shunmugam <113205555+K-Senthil-Shunmugam@users.noreply.github.com>
2024-01-20 02:57:13 +0530

    Update model_predictor.py

--- model_predictor.py
+++ model_predictor.py
@@ -6,7 +6,7 @@ import sys
 import os
 import pandas as pd
 import numpy as np
-from sklearn.externals import joblib
+import joblib
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report
